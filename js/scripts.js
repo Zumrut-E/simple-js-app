5c9bdf682b13daa1d6d65982a3ce7df45eca91e4
Zumrut-E <zumrut_ekinci@yahoo.ca>
2023-12-05 01:34:57 +0100

    an array with three objects are created

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'pasta';
-
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Muk', height: 1.2, types: ['poison'] },
+    { name: 'Charizad', height: 1.7, types: ['fire', 'flying'] },
+    { name: 'Smoochum', height: 0.4, types: ['psychic', 'ice'] }
+];
