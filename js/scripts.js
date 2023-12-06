4e67c23f41eac027f075a6169b2693475f992702
Zumrut-E <zumrut_ekinci@yahoo.ca>
2023-12-07 00:12:57 +0100

    Message

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ let pokemonList = [
     { name: 'Charizad', height: 1.7, types: ['fire', 'flying'] },
     { name: 'Smoochum', height: 0.4, types: ['psychic', 'ice'] }
 ];
+
+// Exercise Part 1
+/* for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " ");
+} */
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " " + " -Wow, That is big!" + " ")
+    } else {
+        document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " ");
+    }
+}
