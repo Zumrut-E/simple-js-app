4dbbfe894be3f6a6b62c5ff257818ec3bd7fdf9f
Zumrut-E <zumrut_ekinci@yahoo.ca>
2023-12-13 21:16:27 +0100

    using forEach instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,39 @@ let pokemonList = [
 /* for (let i = 0; i < pokemonList.length; i++) {
     document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " ");
 } */
-
+/* 
 for (let i = 0; i < pokemonList.length; i++) {
     if (pokemonList[i].height > 1.5) {
         document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " " + " -Wow, That is big!" + " ")
     } else {
         document.write(pokemonList[i].name + " " + "height: " + pokemonList[i].height + " ");
     }
+} */
+
+
+pokemonList.forEach(function (pokemon) {
+    document.write(pokemon.name + " " + "height: " + pokemon.height + " " + "type: " + pokemon.types + " ");
+});
+
+
+document.write("<br>");
+
+pokemonList.forEach(pokemon => {
+    if (pokemon.height > 1.5) {
+        document.write(`${pokemon.name} height: ${pokemon.height} -Wow, That is big! type: ${pokemon.types} `);
+    } else {
+        document.write(`${pokemon.name} height: ${pokemon.height} type: ${pokemon.types} `);
+    }
+});
+
+
+function divide(dividend, divisor) {
+    if (divisor === 0) {
+        return "You are trying to divided by zero"
+    } else {
+        let result = dividend / divisor;
+        return result;
+    }
 }
+
+console.log(divide(4, 2));
